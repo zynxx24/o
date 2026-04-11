b63dcbc85954af58b26d3aa4b915e7b907c240fd
Zyn <136204040+zynxx24@users.noreply.github.com>
2026-04-11 19:48:42 +0800

    Update UserFactory.php

--- CateringKu/database/factories/UserFactory.php
+++ CateringKu/database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'username' => fake()->unique()->userName(),
+            'username' => fake()->unique()->lexify('user_????'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
